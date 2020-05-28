01c788103f1f1431a7c6bc75780a19bb66de33d8
valeffe1109 <valeriofusco1109v@gmail.com>
2020-05-28 15:36:44 +0200

    Routes Buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Register from './components/Register/index';
 
 const App = () =>{
   return (
     <div className="App">
-     
+     <Register/>
     </div>
   );
 }
---
+++ src/components/Register/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button} from 'react-bootstrap';
+
+const index = () => {
+    return(
+        <div>
+        <Button>Developer</Button>
+        <Button>Company</Button>
+        </div>
+    )
+}
+
+export default index;
